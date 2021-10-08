eb0b6750e36ed1a97573c1af403f24f40986e285
Nathan-Cahill <87508548+Nathan-Cahill@users.noreply.github.com>
2021-10-08 13:02:52 +1100

    added notification for Jordan

--- LetsChatLiving/public/js/main.js
+++ LetsChatLiving/public/js/main.js
@@ -13,21 +13,44 @@ const Toast = {
     document.body.appendChild(this.el);
   },
 
-  show(message, state){
+  // show(message, state){
+  //   clearTimeout(this.hideTimeout);
+
+  //   this.el.textContent = message;
+  //   this.el.className = 'toast toast--visible';
+
+  //   if(state){
+  //     this.el.classList.add(`toast--${state}`)
+  //   }
+
+  //   this.hideTimeout = setTimeout(() => {
+  //       this.el.classList.remove('toast--visible')
+  //   } ,3000);
+  // }
+};
+
+const showMessage = (message) =>{
+  // show(message, state){
     clearTimeout(this.hideTimeout);
 
     this.el.textContent = message;
     this.el.className = 'toast toast--visible';
-
-    if(state){
-      this.el.classList.add(`toast--${state}`)
-    }
-
+    this.el.classList.add(`toast--success`)
+    
     this.hideTimeout = setTimeout(() => {
         this.el.classList.remove('toast--visible')
     } ,3000);
-  }
-};
+  // }
+}
+
+// Notification integration with Jordan
+const notificationSocket = io();
+
+notificationSocket.on('toast toast--visible', message =>{
+
+  showMessage(message);
+  
+});
 
 document.addEventListener('DOMContentLoaded', ()=> Toast.init());
 
